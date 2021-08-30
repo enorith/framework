ffe90f31596157c0c25c7b6da58727baadf08d3f
CaoJiayuan <cjy632258@hotmail.com>
2021-08-30 19:31:44 +0800

    update unique rule

--- http/rules/unique.go
+++ http/rules/unique.go
@@ -8,20 +8,28 @@ import (
 )
 
 type Unique struct {
-	db    *gorm.DB
-	table string
-	field string
+	db            *gorm.DB
+	table         string
+	field         string
+	inputFormater func(input contracts.InputValue) interface{}
 }
 
 func (u *Unique) Passes(input contracts.InputValue) (success bool, skipAll bool) {
 	var count int64
-	u.db.Table(u.table).Where(fmt.Sprintf("%s = ?", u.field), input).Count(&count)
+	u.db.Table(u.table).Where(fmt.Sprintf("%s = ?", u.field), u.inputFormater(input)).Count(&count)
 	if count > 0 {
 		return false, false
 	}
 	return true, false
 }
 
+func (u *Unique) FormatInput(f func(input contracts.InputValue) interface{}) *Unique {
+	u.inputFormater = f
+	return u
+}
+
 func NewUnique(db *gorm.DB, table, field string) *Unique {
-	return &Unique{db: db, table: table, field: field}
+	return &Unique{db: db, table: table, field: field, inputFormater: func(input contracts.InputValue) interface{} {
+		return input.GetString()
+	}}
 }
