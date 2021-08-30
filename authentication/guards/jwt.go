88c910f5f987150c0d04a5f05207e307a27a4d8b
CaoJiayuan <cjy632258@hotmail.com>
2021-08-30 19:58:55 +0800

    comment

--- authentication/guards/jwt.go
+++ authentication/guards/jwt.go
@@ -2,15 +2,18 @@ package guards
 
 import "github.com/enorith/http/contracts"
 
+//JWTConfig jwt auth guard config
 type JWTConfig struct {
 	Key string `yaml:"key" env:"AUTH_JWT_KEY"`
 	TTL int    `yaml:"ttl" env:"AUTH_JWT_TTL"`
 }
 
+//TokenProvider of jwt auth guard
 type TokenProvider struct {
 	Request contracts.RequestContract
 }
 
+//GetAccessToken: get request token (bearer)
 func (tp TokenProvider) GetAccessToken() ([]byte, error) {
 	return tp.Request.BearerToken()
 }
--- authentication/manager.go
+++ authentication/manager.go
@@ -8,30 +8,36 @@ import (
 	"github.com/enorith/http/contracts"
 )
 
+//AuthManager default manager
 var AuthManager = &Manager{
 	drivers:   make(map[string]DriverRegister),
 	providers: make(map[string]AuthProvider),
 	mu:        sync.RWMutex{},
 }
 
+//DriverRegister register of auth guard
 type DriverRegister func(up AuthProvider, r contracts.RequestContract) (authenticate.Guard, error)
 
+//GuardConfig config of auth guard
 type GuardConfig struct {
 	Driver   string `yaml:"driver"`
 	Provider string `yaml:"provider"`
 }
 
+//Config of authentication
 type Config struct {
 	Default string                 `yaml:"default"`
 	Guards  map[string]GuardConfig `yaml:"guards"`
 }
 
+//Manager of authentication
 type Manager struct {
 	drivers   map[string]DriverRegister
 	providers map[string]AuthProvider
 	mu        sync.RWMutex
 }
 
+//GetDriverRegister get auth guard driver register
 func (m *Manager) GetDriverRegister(name string) (DriverRegister, bool) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
@@ -40,6 +46,7 @@ func (m *Manager) GetDriverRegister(name string) (DriverRegister, bool) {
 	return dr, ok
 }
 
+//GetProvider get auth provider
 func (m *Manager) GetProvider(name string) (AuthProvider, bool) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
@@ -47,18 +54,22 @@ func (m *Manager) GetProvider(name string) (AuthProvider, bool) {
 
 	return p, ok
 }
+
+//WithProvider register auth provider
 func (m *Manager) WithProvider(name string, p AuthProvider) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	m.providers[name] = p
 }
 
+//RegisterDriver register auth guard driver
 func (m *Manager) RegisterDriver(name string, dr DriverRegister) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	m.drivers[name] = dr
 }
 
+//GetGuard get auth guard
 func (m *Manager) GetGuard(driver, provider string, r contracts.RequestContract) (authenticate.Guard, error) {
 
 	dr, ok := m.GetDriverRegister(driver)
