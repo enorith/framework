f528150250d69f0259bacef4a44211fa11353d08
Nerio <cjy632258@hotmail.com>
2021-09-12 20:40:54 +0800

    update config

--- app.go
+++ app.go
@@ -30,7 +30,7 @@ type Config struct {
 	Locale   string `yaml:"locale" env:"APP_LOCALE" default:"en"`
 	Url      string `yaml:"url" env:"APP_URL" default:"http://localhost"`
 	Timezone string `yaml:"timezone" env:"APP_TIMEZONE" default:""`
-	Key      string `yaml:"timezone" env:"APP_KEY" default:"somerandomkey!!!"`
+	Key      string `yaml:"key" env:"APP_KEY" default:"somerandomkey!!!"`
 }
 
 //App: framework application
